8a028b7296a9d679490f4b78064871a35ccd0fc0
21021123 <21021123@LAPTOP-G1P1OE4Q>
2022-08-10 22:03:40 +0800

    update version

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 
 public class C206_CaseStudy {
-		
+	private static final int OPTION_DELETE = 4;
+	private static final int OPTION_ADD = 1;
+	private static final int OPTION_VIEW = 2;
+	private static final int OPTION_UPDATE= 3;
+	private static final int OPTION_QUIT = 5;
 		public static void main(String[] args) {
 
 			ArrayList<Menu>menuList = new ArrayList<Menu>();
@@ -13,35 +17,24 @@ public class C206_CaseStudy {
 			menuList(new Menu("m303", "Vegetarian Chicken Rice, fruit juice, papaya"));
 			
 			int option = 0;
-			
-			while (option != 4) {
-				Helper.line(40, "=");
-				System.out.println("1. View Menulist");
-				System.out.println("2. Add Menu Items");
-				System.out.println("3. Delete Menu Items");
-				System.out.println("4. Quit");
-				
-				option = Helper.readInt("Enter your option: ");
-				
-				if (option == 1) {
-					viewAll(menuList);
-				}
-				
-				else if (option == 2) {
-					addAll(menuList);
-				}
-				
-				else if (option == 3) {
-					deleteAll(menuList);	
-				}
-				
-				else if (option == 4) {
-					System.out.println("quit");
-				}
-				
-				else {
-					System.out.println("Invalid option");
-				}
+
+			while (option != OPTION_QUIT) {
+
+				C206_CaseStudy.menu();
+				option = Helper.readInt("Enter an option > ");
+
+				if (option == OPTION_VIEW) {
+					// View all items
+					C206_CaseStudy.viewAllMenu(menuList);
+					C206_CaseStudy.viewAllAccount(accountList);
+					C206_CaseStudy.viewAllOrder(orderList);
+
+				} else if (option == OPTION_ADD) {
+					// Add a new item
+					C206_CaseStudy.setHeader("ADD");			
+				}else if(option == OPTION_UPDATE) {
+					
+				}else if (option == OPTION_DELETE);
 			}
 		}
 
